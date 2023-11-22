83420058a2cbd2503c178f3ce4a2a79670e8d004
kmeri <kmerisushma@gmail.com>
2023-11-22 22:43:23 +0530

    making shell scripting topics

--- frontend.sh
+++ frontend.sh
@@ -1,5 +1,6 @@
 dnf install nginx -y
-
+systemctl enable nginx
+systemctl restart nginx
 cp expense.conf /etc/nginx/default.d/expense.conf
 
 rm -rf /usr/share/nginx/html/*
@@ -9,5 +10,3 @@ curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zi
 cd /usr/share/nginx/html
 unzip /tmp/frontend.zip
 
-systemctl enable nginx
-systemctl restart nginx
