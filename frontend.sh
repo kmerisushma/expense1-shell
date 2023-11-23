04f03d81a44246b8e1ba3f5eb5241bf6ed546917
kmeri <kmerisushma@gmail.com>
2023-11-23 16:28:56 +0530

    making shell scripting topics

--- frontend.sh
+++ frontend.sh
@@ -3,29 +3,30 @@ color="\e[36m"
 
 echo -e "${color} Installing Nginx \e[0m"
 dnf install nginx -y &>>log_file
+echo $?
 
 echo -e "${color} Copy Expense Config file \e[0m"
 cp expense.conf /etc/nginx/default.d/expense.conf &>>log_file
-
+echo $?
 
 echo -e "${color} clean old Nginx \e[0m"
 rm -rf /usr/share/nginx/html/* &>>log_file
-
+echo $?
 
 echo -e "${color} download frontend application code \e[0m"
 curl -o /tmp/frontend.zip https://expense-artifacts.s3.amazonaws.com/frontend.zip &>>log_file
-
+echo $?
 
 echo -e "${color} Extract Downloaded application Content \e[0m"
 cd /usr/share/nginx/html &>>log_file
  unzip /tmp/frontend.zip &>>log_file
-
+echo $?
 
 echo -e "${color} Starting Nginx Service  \e[0m"
 systemctl enable nginx &>>log_file
 
 systemctl restart nginx &>>log_file
-
+echo $?
 
 
 
