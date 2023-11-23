44dc86a2fb2b470e3127f101f8dd1301fdb2ab5b
kmeri <kmerisushma@gmail.com>
2023-11-23 20:17:49 +0530

    making shell scripting topics

--- backend.sh
+++ backend.sh
@@ -1,25 +1,53 @@
+log_file="/tmp/expense.log"
+color="\e[33m"
+
+echo -e "${color} Disable NodeJS default version \e[0m"
 dnf module disable nodejs -y
+echo $?
+
+echo -e "${color} Enable NodeJS 18  version \e[0m"
 dnf module enable nodejs:18 -y
+echo $?
 
+echo -e "${color} Install NodeJS  \e[0m"
 dnf install nodejs -y
+echo $?
 
+echo -e "${color} Copy Backend service File \e[0m"
 cp backend.service /etc/systemd/system/backend.service
+echo $?
 
+echo -e "${color} Add Application user \e[0m"
 useradd expense
+echo $?
 
+echo -e "${color} create application directory \e[0m"
 mkdir /app
+echo $?
 
+echo -e "${color} Download application content \e[0m"
 curl -o /tmp/backend.zip https://expense-artifacts.s3.amazonaws.com/backend.zip
+echo $?
+
+echo -e "${color} Extract application content \e[0m"
 cd /app
 unzip /tmp/backend.zip
+echo $?
 
-cd /app
+echo -e "${color} download NodeJS Dependencies \e[0m"
 npm install
+echo $?
 
+echo -e "${color} Install MySQL client to load schema \e[0m"
 dnf install mysql -y
+echo $?
+
+echo -e "${color} Load Schema \e[0m"
 mysql -h mysql-dev.sushma143.online -uroot -pExpenseApp@1 < /app/schema/backend.sql
+echo $?
 
-
+echo -e "${color} Starting backend service \e[0m"
 systemctl deamon-reload
 systemctl enable backend
 systemctl restart backend
+echo $?
--- mysql.sh
+++ mysql.sh
@@ -1,12 +1,25 @@
-dnf module disable mysql -y
+log_file=/tmp/expense.log
+color="\e[36m"
 
-cp mysql.repo /etc/yum.repos.d/mysql.repo
+echo -e "${color} Disable MySQL default version \e[0m"
+dnf module disable mysql -y &>>$log_file
+echo $?
 
-dnf install mysql-community-server -y
+echo -e "${color} Copy MySQL Repo file \e[0m"
+cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
+echo $?
 
-systemctl enable mysqld
-systemctl start mysqld
+echo -e "${color} Installing MySQL server\e[0m"
+dnf install mysql-community-server -y &>>$log_file
+echo $?
 
-mysql_secure_installation --set-root-pass ExpenseApp@1
+echo -e "${color} Start MySQL server \e[0m"
+systemctl enable mysqld &>>$log_file
+systemctl start mysqld &>>$log_file
+echo $?
+
+echo -e "${color} set MySQL password \e[0m"
+mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
+echo $?
 
 
