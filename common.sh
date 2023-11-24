4046ebd81a2e47a3657f7b04d77192f71cb05924
kmeri <kmerisushma@gmail.com>
2023-11-24 19:06:42 +0530

    making shell scripting topics

--- common.sh
+++ common.sh
@@ -1,2 +1,10 @@
 log_file="/tmp/expense.log"
 color="\e[33m"
+
+status_check(){
+ if [ $? -eq 0 ]; then
+    echo -e "\e[32m SUCCESS \e[0m"
+   else
+     echo -e "\e[31m FAIL \e[0m"
+ fi
+}
--- mysql.sh
+++ mysql.sh
@@ -1,25 +1,23 @@
-log_file=/tmp/expense.log
-color="\e[36m"
+source common.sh
 
 echo -e "${color} Disable MySQL default version \e[0m"
 dnf module disable mysql -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Copy MySQL Repo file \e[0m"
 cp mysql.repo /etc/yum.repos.d/mysql.repo &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Installing MySQL server\e[0m"
 dnf install mysql-community-server -y &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} Start MySQL server \e[0m"
 systemctl enable mysqld &>>$log_file
 systemctl start mysqld &>>$log_file
-echo $?
+status_check
 
 echo -e "${color} set MySQL password \e[0m"
-mysql_secure_installation --set-root-pass ExpenseApp@1 &>>$log_file
-echo $?
+status_check
 
 
